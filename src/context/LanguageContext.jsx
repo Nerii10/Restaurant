8b88bb106a4e0379bbb5a55eaa164818d5040c0c
nerii <96701056+Nerii10@users.noreply.github.com>
2025-05-31 13:52:02 +0200

    e

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState("de");
+  const [language, setLanguage] = useState("pl");
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
